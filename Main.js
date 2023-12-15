4124a5a463abcb3860383f90799a11b55d8bd811
Alden Jenish <aldenjenish@Aldens-MacBook-Air.local>
2023-12-15 19:28:29 +0530

    Files Added

---
+++ Main.js
@@ -0,0 +1,30 @@
+const notesContainer = document.querySelector(".notes");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelector(".input-box");
+
+function storage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", ()=>{
+    let intputBox = document.createElement("p");
+    let img = document.createElement("img");
+    intputBox.className = "input-box";
+    intputBox.setAttribute("contenteditable","true");
+    img.src = "del.png";
+    notesContainer.appendChild(intputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        storage();
+    } else if(e.target.tagName === "P"){
+        notes = document.querySelector(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                storage();
+            }
+        })
+    }
+})
